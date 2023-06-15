5caaa09345fafe16f8783139f8d2c954dceeba66
Michael Perkins <mperkins1995@gmail.com>
2023-06-15 07:36:15 -0400

    add cardsGrid styling

--- memory-game/src/components/CardsGrid/CardsGrid.js
+++ memory-game/src/components/CardsGrid/CardsGrid.js
@@ -12,8 +12,17 @@ const CardsGrid = ({ characters, handleCardClick }) => {
 
 const CardsGridWrapper = styled.div`
   display: grid;
-  grid-template-columns: 200px 200px 200px 200px;
-  column-gap: 10px;
-`;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 3rem;
+
+  @media (max-width: 750px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: 550px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`
+
 
 export default CardsGrid;
