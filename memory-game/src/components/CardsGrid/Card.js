3f67f961faabcdf6759634bf07d5af3319322bfc
Michael Perkins <mperkins1995@gmail.com>
2023-06-15 07:35:26 -0400

    added card styling

--- memory-game/src/components/CardsGrid/Card.js
+++ memory-game/src/components/CardsGrid/Card.js
@@ -4,26 +4,38 @@ import styled from "styled-components";
 const Card = ({ character, handleCardClick }) => {
   return (
     <CardWrapper onClick={handleCardClick}>
-      <img src={character.image} alt={character.name} />
-      <p>{character.name}</p>
+      <Image src={character.image} alt={character.name} />
+      <Name>{character.name}</Name>
     </CardWrapper>
   );
 };
 
 const CardWrapper = styled.div`
-  background: lightgrey;
-  width: 200px;
   display: flex;
   flex-direction: column;
-  margin: 5px;
-  padding: 5px;
-  align-items: center;
-  border: 1px solid black;
+  width: 150px;
+  border-radius: 20px;
+  background-color: lightgrey;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  transition: transform 0.3s;
+  cursor: pointer;
 
-  img {
-    width: 150px;
-    height: 150px;
+  &:hover {
+    transform: scale(1.05);
   }
-`;
+`
+
+const Image = styled.img`
+  padding: 0.5rem;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+`
+
+const Name = styled.p`
+  padding: 0.5rem;
+  font-size: 1rem;
+  font-weight: 700;
+  text-align: center;
+`
 
 export default Card;
