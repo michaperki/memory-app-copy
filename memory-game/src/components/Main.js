ca13956f213ee91ee8cdbebf7e606784114cbf31
Michael Perkins <mperkins1995@gmail.com>
2023-06-15 06:49:42 -0400

    implemented playRound and resetGame and tied to scoreboard

--- memory-game/src/components/Main.js
+++ memory-game/src/components/Main.js
@@ -7,6 +7,9 @@ import { shuffleArray } from "../utils";
 const Main = () => {
   const CHARACTER_NUM = 12;
   const [characters, setCharacters] = useState([]);
+  const [clickedCharacters, setClickedCharacters] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
   useEffect(() => {
     const loadCards = async () => {
@@ -32,14 +35,32 @@ const Main = () => {
 
   const handleCardClick = (e) => {
     const characterName = e.target.parentNode.lastChild.textContent;
-    console.log(characterName);
-    setCharacters(shuffleArray(characters))
-  }
+    playRound(characterName);
+    setCharacters(shuffleArray(characters));
+  };
+
+  const playRound = (characterName) => {
+    if (clickedCharacters.includes(characterName)) {
+      resetGame();
+    } else {
+      const newScore = currentScore + 1;
+      if (newScore > bestScore) {
+        setBestScore(newScore);
+      }
+      setCurrentScore(newScore);
+      setClickedCharacters((prevState) => [...prevState, characterName]);
+    }
+  };
+
+  const resetGame = () => {
+    setClickedCharacters([]);
+    setCurrentScore(0);
+  };
 
   return (
     <>
-      <Scoreboard score={2} bestScore={3} />
-      <CardsGrid characters={characters} handleCardClick={handleCardClick}/>
+      <Scoreboard score={currentScore} bestScore={bestScore} />
+      <CardsGrid characters={characters} handleCardClick={handleCardClick} />
     </>
   );
 };
